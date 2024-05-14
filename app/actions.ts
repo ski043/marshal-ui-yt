3e1c64410b9bef554fb40517abe46f04a5dc968e
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2024-05-14 18:38:43 +0200

    added correct url

--- app/actions.ts
+++ app/actions.ts
@@ -170,8 +170,14 @@ export async function BuyProduct(formData: FormData) {
         destination: data?.User?.connectedAccountId as string,
       },
     },
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000/payment/cancel",
+    success_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/success"
+        : "https://marshal-ui-yt.vercel.app/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/cancel"
+        : "https://marshal-ui-yt.vercel.app/payment/cancel",
   });
 
   return redirect(session.url as string);
@@ -197,8 +203,14 @@ export async function CreateStripeAccoutnLink() {
 
   const accountLink = await stripe.accountLinks.create({
     account: data?.connectedAccountId as string,
-    refresh_url: `http://localhost:3000/billing`,
-    return_url: `http://localhost:3000/return/${data?.connectedAccountId}`,
+    refresh_url:
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:3000/billing`
+        : `https://marshal-ui-yt.vercel.app/billing`,
+    return_url:
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:3000/return/${data?.connectedAccountId}`
+        : `https://marshal-ui-yt.vercel.app/return/${data?.connectedAccountId}`,
     type: "account_onboarding",
   });
 
--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -48,5 +48,9 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://marshal-ui-yt.vercel.app/"
+  );
 }
